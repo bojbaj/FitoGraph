385be3065bc3bbf615d7b6f6fbd81ac4ebc5dcbd
AlirezA Az <bojbaj@gmail.com>
2020-08-22 21:35:53 +0430

    fix loop handler in json serializer

--- src/FitoGraph.Api/Infrastructure/StringExtentions.cs
+++ src/FitoGraph.Api/Infrastructure/StringExtentions.cs
@@ -1,10 +1,16 @@
+using Newtonsoft.Json;
+
 namespace FitoGraph.Api.Infrastructure
 {
     public static class StringExtentions
     {
         public static string ToJsonString<T>(this T obj)
         {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            JsonSerializerSettings settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj, settings);
         }
     }
 }
