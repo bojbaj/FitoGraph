3ed36aec7a53390baf0f3bb6246d9d1c6c1572ad
AlirezA Az <bojbaj@gmail.com>
2020-11-22 19:13:55 +0330

    firebase projectId added to config

--- src/FitoGraph.Api/Helpers/Settings/AppSettingFireBase.cs
+++ src/FitoGraph.Api/Helpers/Settings/AppSettingFireBase.cs
@@ -3,5 +3,6 @@ namespace FitoGraph.Api.Helpers.Settings
     public class AppSettingFireBase
     {
         public string ApiKey { get; set; }
+        public string ProjectId { get; set; }
     }
 }
